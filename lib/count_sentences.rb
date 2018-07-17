97a28d1375cd3d1094ecd7c93f9c943c85831e3f
Hiroki Kato <hkato@sas.upenn.edu>
2018-07-17 17:26:54 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,6 +16,6 @@ class String
 
   def count_sentences
     binding.pry 
-    self.split(/[.?!]/).delete_if{|x| x.size < 2}.size
+    self.split(/\.|\?|\!/).delete_if{|x| x.size < 2}.size
   end
 end
